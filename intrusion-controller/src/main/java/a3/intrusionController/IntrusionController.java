92f724f146ed787ebc4555f3e14381936cfdb266
Weinan Qiu <davidiamyou@gmail.com>
2016-03-28 20:55:32 -0400

    created intrusion sensor and controller

---
+++ intrusion-controller/src/main/java/a3/intrusionController/IntrusionController.java
@@ -0,0 +1,8 @@
+package a3.intrusionController;
+
+/**
+ * @author Weinan Qiu
+ * @since 1.0.0
+ */
+public class IntrusionController {
+}
---
+++ intrusion-sensor/src/main/java/a3/intrusionSensor/IntrusionSensor.java
@@ -0,0 +1,8 @@
+package a3.intrusionSensor;
+
+/**
+ * @author Weinan Qiu
+ * @since 1.0.0
+ */
+public class IntrusionSensor {
+}
